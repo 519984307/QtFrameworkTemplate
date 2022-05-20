a22ebf22e4389dcbb6b5b4dcd71facf7d51a27ee
gyf <405126907@qq.com>
2022-05-20 16:50:34 +0800

    备份

---
+++ src/interfaces/MainWidget/imainwidget.h
@@ -0,0 +1,35 @@
+﻿#ifndef IMAINWIDGET_H
+#define IMAINWIDGET_H
+
+#include <QtGlobal>
+#include <skinmanager.h>
+
+#define MAINWIDGET_UUID "{89586123-e83d-451c-a1d5-84ff78b76d79}"
+
+
+class IMainWidget
+{
+public:
+    virtual QWidget *instance() = 0;
+
+
+//添加外部模块需要访问的接口
+
+
+};
+
+class IMainWidgetPlugin
+{
+public:
+    virtual QObject *instance() = 0;
+    virtual IMainWidget *mainWidget() const = 0;
+    virtual void showMainWidget() = 0;
+
+protected:
+    virtual void mainWidgetCreated() = 0;
+    virtual void aboutToSignOut() = 0;
+};
+
+Q_DECLARE_INTERFACE(IMainWidget,"QtFrameworkTemplate.Plugin.IMainWidget/1.0")
+Q_DECLARE_INTERFACE(IMainWidgetPlugin,"QtFrameworkTemplate.Plugin.IMainWidgetPlugin/1.0")
+#endif
--- src/interfaces/interfaces.pri
+++ src/interfaces/interfaces.pri
@@ -2,4 +2,6 @@ HEADERS += iglobalsetting.h \
            ipluginmanager.h \
            $$PWD/MainWidget/imainwidget.h
 
+SOURCES +=interfaces.cpp    #为了生成dll,创建空cpp
+
 
